ba24a3a940a55b56bf759cb7414de1245c144f06
NekrodNIK <60639354+nekrodnik@users.noreply.github.com>
2025-10-09 22:08:08 +0700

    console refactoring

---
+++ src/console/mod.rs
@@ -0,0 +1,120 @@
+use crate::io;
+
+mod vga;
+
+pub struct Console {
+    output: vga::TextMode80x25,
+    state: State,
+}
+
+struct State {
+    pub x: usize,
+    pub y: usize,
+    pub fg: vga::Color,
+    pub bg: vga::Color,
+}
+
+impl Console {
+    pub fn new() -> Self {
+        Self {
+            output: vga::TextMode80x25::new(),
+            state: State {
+                x: 0,
+                y: 0,
+                fg: vga::DEFAULT_COLORCODE.get_fg(),
+                bg: vga::DEFAULT_COLORCODE.get_bg(),
+            },
+        }
+    }
+
+    pub fn clear(&mut self) {
+        self.output.clear()
+    }
+}
+
+impl io::Write for Console {
+    fn write_all(&mut self, buf: &[u8]) -> Result<(), io::Error> {
+        let mut iter = buf.iter();
+        while let Some(byte) = iter.next() {
+            if *byte == b'\n' {
+                self.state.x = 0;
+                self.state.y += 1;
+            } else {
+                // TODO: add more complete support ANSI escape codes
+                // FIXME: improve readability
+                if *byte == 0x1b {
+                    if let Some(byte) = iter.next()
+                        && *byte == b'['
+                    {
+                        if let (Some(byte1), Some(byte2), Some(byte3)) =
+                            (iter.next(), iter.next(), iter.next())
+                            && *byte3 == b'm'
+                        {
+                            let color_escape = &[*byte1, *byte2];
+
+                            enum Command {
+                                ColorFg(vga::Color),
+                                ColorBg(vga::Color),
+                                DefaultFg,
+                                DefaultBg,
+                                Skip,
+                            }
+
+                            let selected = match color_escape {
+                                b"30" => Command::ColorFg(vga::Color::Black),
+                                b"31" => Command::ColorFg(vga::Color::Red),
+                                b"32" => Command::ColorFg(vga::Color::Green),
+                                b"33" => Command::ColorFg(vga::Color::Yellow),
+                                b"34" => Command::ColorFg(vga::Color::Blue),
+                                b"35" => Command::ColorFg(vga::Color::Magenta),
+                                b"36" => Command::ColorFg(vga::Color::Cyan),
+                                b"37" => Command::ColorFg(vga::Color::White),
+                                b"39" => Command::DefaultFg,
+
+                                b"40" => Command::ColorBg(vga::Color::Black),
+                                b"41" => Command::ColorBg(vga::Color::Red),
+                                b"42" => Command::ColorBg(vga::Color::Green),
+                                b"43" => Command::ColorBg(vga::Color::Yellow),
+                                b"44" => Command::ColorBg(vga::Color::Blue),
+                                b"45" => Command::ColorBg(vga::Color::Magenta),
+                                b"46" => Command::ColorBg(vga::Color::Cyan),
+                                b"47" => Command::ColorBg(vga::Color::White),
+                                b"49" => Command::DefaultBg,
+                                _ => Command::Skip,
+                            };
+
+                            match selected {
+                                Command::ColorFg(color) => self.state.fg = color,
+                                Command::ColorBg(color) => self.state.bg = color,
+                                Command::DefaultFg => {
+                                    self.state.fg = vga::DEFAULT_COLORCODE.get_fg()
+                                }
+                                Command::DefaultBg => {
+                                    self.state.bg = vga::DEFAULT_COLORCODE.get_bg()
+                                }
+                                _ => (),
+                            }
+                        }
+                    }
+                    continue;
+                }
+
+                let character =
+                    vga::Character::new(*byte, vga::ColorCode::new(self.state.fg, self.state.bg));
+                self.output
+                    .set_character(character, self.state.x, self.state.y);
+
+                self.state.x += 1;
+                self.state.y += self.state.x / vga::TextMode80x25::WIDTH;
+                self.state.x %= vga::TextMode80x25::WIDTH;
+            }
+
+            if self.state.y >= vga::TextMode80x25::HEIGHT {
+                self.output.scroll_down();
+                self.state.y -= 1;
+            }
+        }
+
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,37 @@
 #![no_std]
 #![no_main]
 
+mod ansi_escape;
+mod console;
 mod io;
 mod irq;
 mod logs;
 mod utils;
-mod vga;
 
 use core::cell::LazyCell;
 use core::panic::PanicInfo;
 
+use crate::console::Console;
 use crate::io::Write;
 use crate::irq::IrqSafe;
-use crate::vga::Vga;
 
 const LOGO: &'static str = include_str!("logo.txt");
 
-static VGA: IrqSafe<LazyCell<Vga>> = IrqSafe::new(LazyCell::new(|| Vga::new()));
+static CONSOLE: IrqSafe<LazyCell<Console>> = IrqSafe::new(LazyCell::new(|| Console::new()));
 
 #[unsafe(no_mangle)]
 pub extern "C" fn kmain() -> ! {
-    let mut vga = VGA.lock();
-    vga.clear();
-    logs::info!(vga, "{}", "Loading system...");
-    write!(vga, "{}\n", LOGO).unwrap();
+    let mut console = CONSOLE.lock();
+    console.clear();
+    write!(console, "{}\n", LOGO).unwrap();
+    logs::info!(console, "{}", "Loading system...");
+
     loop {}
 }
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    let mut vga = VGA.lock();
-    logs::panic!(vga, "{}", info.message());
+    let mut console = CONSOLE.lock();
+    logs::panic!(console, "{}", info.message());
     loop {}
 }
